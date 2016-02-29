64d6897606e99398a9c53d78ff393c744b8c64ea
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-02-29 09:23:45 -0600

    first 4 tests passing

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,11 +2,20 @@ class Hamming
 
   def self.compute(arg1, arg2)
     #count of characters that differ at same index
+    #string inside array?
+    #
+
     if arg1.eql?(arg2)
       0
     else
+      array1 = arg1.split('')
+      array2 = arg2.split('')
+      array1.length.times do |x|
+        if array1[x] != array2[x]
+         1
+        end
+      end
     end
-
   end
 
 end
--- ruby/hamming/hamming_test.rb
+++ ruby/hamming/hamming_test.rb
@@ -16,17 +16,16 @@ class HammingTest < Minitest::Test
   end
 
   def test_complete_distance_in_single_nucleotide_strands
-    skip
     assert_equal 1, Hamming.compute('A', 'G')
   end
 
   def test_complete_distance_in_small_strands
-    skip
+    # skip
     assert_equal 2, Hamming.compute('AG', 'CT')
   end
 
   def test_small_distance_in_small_strands
-    skip
+    # skip
     assert_equal 1, Hamming.compute('AT', 'CT')
   end
 
