84a02b021b6dc0d5736f5810d7da63181a9c62b9
mmdotz <dotzenrodm@gmail.com>
2018-05-14 22:26:19 -0500

    test2 failing exponents

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -21,14 +21,14 @@ require 'prime'
 class Raindrops
 
   def self.convert(number)
-    # raindrop_speak = {3=>"Pling", 5=>"Plang", 7=>"Plong"}
 
-    factors = number.prime_division.flatten.uniq.sort.map(&:to_s).keep_if { |x| x =~ /[357]/ }
+    factors = number.prime_division.flatten.uniq.sort.keep_if { |x| [3,5,7].include?(x) }
+
     raindrop_speak = ""
     if factors.empty?
-      raindrop_speak << number
+      raindrop_speak << number.to_s
     else
-      factors.each do |factor|
+      factors.map(&:to_s).each do |factor|
         case factor
         when "3"
           raindrop_speak << "Pling"
@@ -47,16 +47,6 @@ module BookKeeping
   VERSION = 3
 end
 
-
-#   1) Failure:
-# RaindropsTest#test_the_sound_for_1_is_1 [raindrops/raindrops_test.rb:8]:
-# Expected: "1"
-#   Actual: "\u0001"
-
-#   2) Failure:
-# RaindropsTest#test_the_sound_for_52_is_52 [raindrops/raindrops_test.rb:83]: WHY "DOES THIS KEEP 13?"
-# Expected: "52"
-#   Actual: ""
 
 #   3) Failure:
 # RaindropsTest#test_2_to_the_power_3_does_not_make_a_raindrop_sound_as_3_is_the_exponent_not_the_base [raindrops/raindrops_test.rb:33]:
