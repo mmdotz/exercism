0bfcd5e4cb431b6b1680e2af89c1a5da0cdd822f
mmdotz <dotzenrodm@gmail.com>
2018-05-20 15:15:31 -0500

    passing all tests, complete refactor

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -16,29 +16,14 @@
 # - 34 has four factors: 1, 2, 17, and 34.
 #   - In raindrop-speak, this would be "34". -->
 
-require 'prime'
-
 class Raindrops
 
   def self.convert(number)
-
-    factors = number.prime_division.flatten.uniq.sort.keep_if { |x| [3,5,7].include?(x) }
-
     raindrop_speak = ""
-    if factors.empty?
-      raindrop_speak << number.to_s
-    else
-      factors.map(&:to_s).each do |factor|
-        case factor
-        when "3"
-          raindrop_speak << "Pling"
-        when "5"
-          raindrop_speak << "Plang"
-        when "7"
-          raindrop_speak << "Plong"
-        end
-      end
-    end
+    raindrop_speak << "Pling" if number % 3 == 0
+    raindrop_speak << "Plang" if number % 5 == 0
+    raindrop_speak << "Plong" if number % 7 == 0
+    raindrop_speak << number.to_s if raindrop_speak.empty?
     raindrop_speak
   end
 end
@@ -47,8 +32,3 @@ module BookKeeping
   VERSION = 3
 end
 
-
-#   3) Failure:
-# RaindropsTest#test_2_to_the_power_3_does_not_make_a_raindrop_sound_as_3_is_the_exponent_not_the_base [raindrops/raindrops_test.rb:33]:
-# Expected: "8"
-#   Actual: "Pling"
